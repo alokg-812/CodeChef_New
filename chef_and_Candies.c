abd135ee1c4517efc9cfa5b7bcb0c412164989a2
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-03-17 23:59:44 +0530

    candies modified

--- chef_and_Candies.c
+++ chef_and_Candies.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int min_packets_needed(int N, int X) {
-    int candies_needed = N - X;
-    if(candies_needed <= 0 ) return 0;
+int min_packs(int N, int X) {
+    int c_need = N - X;
+    if(c_need <= 0 ) return 0;
     else {
-        int packets_needed = candies_needed / 4;
-        if (candies_needed % 4 != 0) {
-        packets_needed += 1;}
-        return packets_needed;
+        int p_needed = c_need / 4;
+        if (c_need % 4 != 0) {
+        p_needed += 1;}
+        return p_needed;
     }
 }
 int main() {
@@ -15,7 +15,8 @@ int main() {
     for (int i = 0; i < T; ++i) {
         int N, X;
         scanf("%d %d", &N, &X);
-        printf("%d\n", min_packets_needed(N, X));
+        int z = min_packs(N, X);
+        printf("%d\n", z);
     }
         return 0;
 }
